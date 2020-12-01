e31c5cf4af9c5e5cf3a0663fe0cdca06114e93d6
Denis Zargishiiev <NikLageon@gmail.com>
2020-12-01 23:18:10 +0300

    first

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,33 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("cat")
+    private Animal animal1;
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal2;
+
+    @Autowired
+    private Timer timer;
+
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal1.toString() +", " + animal2.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
